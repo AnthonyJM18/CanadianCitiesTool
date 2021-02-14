8afa07d468e2762468f805045273efb0a1c05790
zinchop <zina_long@hotmail.com>
2021-02-14 16:07:12 -0500

    Added newton soft bugget for json added city helper class coded ParseJSON

---
+++ Project1_Group_4/Classes/CityHelper.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project1_Group_4.Classes
+{
+    class CityHelper
+    {
+        public string city { get; set; }
+        public string city_ascii { get; set; }
+        public decimal lat { get; set; }
+        public decimal lng { get; set; }
+        public string country { get; set; }
+        public string admin_name { get; set; }
+        public string capital { get; set; }
+        public int population { get; set; }
+        public int id { get; set; }
+    }
+}
