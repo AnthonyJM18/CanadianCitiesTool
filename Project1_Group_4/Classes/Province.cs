f7fd49da7e076e0f2a0abc0b7249fbee38ea7a08
zinchop <zina_long@hotmail.com>
2021-02-17 12:28:03 -0500

    Added smallest and largest city variables

--- Project1_Group_4/Classes/Province.cs
+++ Project1_Group_4/Classes/Province.cs
@@ -12,5 +12,7 @@ namespace Project1_Group_4.Classes
         public int Population { get; set; }
         public string Capital { get; set; }
         public int NumCities { get; set; }
+        public string SmallestCity { get; set; }
+        public string LargestCity { get; set; }
     }
 }
